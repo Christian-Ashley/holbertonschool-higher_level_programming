c1a3d04d915220e18a3ed5fccda4b5272fbdcc1f
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-16 01:18:09 +0000

    1

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        buffer_list = []
+        for num in my_list:
+            buffer_list.append(num)
+            if not idx < 0 and not idx >= len(my_list):
+            buffer_list[idx] = element
+        return buffer_list
