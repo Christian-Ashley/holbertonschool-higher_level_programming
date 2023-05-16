f02234ba8e167025b34fed137a2b7323602efde8
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-16 22:27:29 +0000

    1

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if isinstance(matrix, list):
+        for dim1 in matrix:  # dimension 1
+            dim1_size = len(dim1)
+            pos = 0
+            for index1 in dim1:
+                if pos >= dim1_size - 1:
+                    print("{:d}".format(index1))
+                else:
+                    print("{:d}".format(index1), end=' ')
+                pos += 1
+    else:
+        print()
+    if pos == 0:  # if the input matrix was empty
+        print()
