0d52d2da2b5041207cfb20b8d1fd5327617720af
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-16 20:21:27 +0000

    1

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-     buffer_string = ""
-     for char in my_string:
-            if char != "c" and char != "C":
-                buffer_string += char
-     return buffer_string
+    buffer_string = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            buffer_string += char
+    return buffer_string
