320e2652728dc52ef928a927acfb6801ea229d51
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-19 16:04:56 +0000

    1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """this class makes sure that size is an integer and tells 
+    """this class makes sure that size is an integer and tells
     you if you fucked that up and how:"""
     def __init__(self, size=0):
         if not isinstance(size, int):
