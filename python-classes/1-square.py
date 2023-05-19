cccde28aefff6a60f29e2b1c733ea0d499db3784
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-19 14:57:34 +0000

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a less empty class for squares:"""
+
+
+class Square:
+    """this class defines squares by size:"""
+    def __init__(self, size):
+        self.__size = size
