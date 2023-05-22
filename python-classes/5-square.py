1473373a768c98d980666c2afce3bd42a2b4738a
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-22 15:23:12 +0000

    1

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -20,9 +20,11 @@ class Square:
         self.__size = value
 
     def area(self):
+"""defines the area:"""
         return self.__size ** 2
 
     def my_print(self):
+"""defines what to print:"""
         if self.__size == 0:
             print()
         else:
