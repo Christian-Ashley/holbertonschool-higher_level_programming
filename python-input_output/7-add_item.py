457b3a2a800a94f7c87e30b8ddcfd7ba3dd50996
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-31 04:23:00 +0000

    1

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-7-add_item
-write a script that adds all arguments to a python list, then save to file
-"""
+""" 7-add_item Module """
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+fname = "add_item.json"
+MyList = []
 
 try:
-        existing_data = load_from_json_file('add_item.json')
+    MyList = load_from_json_file(fname)
 except:
-    existing_data = []
+    pass
 
-new_items = sys.argv[1:]
-updated_data = existing_data + new_items
-
-save_to_json_file(updated_data, 'add_item.json')
+for i in range(1, len(sys.argv)):
+    MyList.append(sys.argv[i])
+save_to_json_file(MyList, fname)
