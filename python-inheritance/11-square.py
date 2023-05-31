9af6b1e7e2eacaa1434775cc5ee83df08f03998d
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-31 03:52:39 +0000

    1

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines squares (subclass of rectangles)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """defines square, subclass of rectangle"""
+
+    def __init__(self, size):
+        """init method"""
+        self.integer_validator("size", size)
+        # super() creates a temporary instance of the obj
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        returns visual representation
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
