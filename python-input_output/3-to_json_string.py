a061f0e28da486268c2043927787c0a11570b86e
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-05-31 04:13:32 +0000

    1

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+func that returns JSON representation of an obj (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    this thing gooo craaaaazy witta json dumps
+    """
+    return json.dumps(my_obj)
