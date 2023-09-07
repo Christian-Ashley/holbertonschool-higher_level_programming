5d6387d2be7434f9690975981033cb4b65e2b207
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-09-07 22:36:37 +0000

    1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,13 +6,12 @@ str2 = "is"
 greater = "and is greater than 5"
 zero = "and is 0"
 less = "and is less than 6 and not 0"
-last_digit = 0
 print(number)
 if number < 0:
     temp_last = abs(number) % 10
     last_digit = temp_last * -1
 else:
-    last_digit = number % 1
+    last_digit = number % 10
 
 if last_digit > 5:
     print(f"{str1} {number} {str2} {last_digit} {greater}")
