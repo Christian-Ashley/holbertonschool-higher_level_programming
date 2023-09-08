0c008a663a495dc6bd2a8c1bd0b5d91425395846
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-09-08 02:13:57 +0000

    1

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-for fizzbuzz in range (101):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print('fizzbuzz')
-        continue
-    elif fizzbuzz % 3 == 0:
-        print('fizz')
-        continue
-    elif fizzbuzz % 5 == 0:
-        print('buzz')
-        continue
-    print(fizzbuzz)
+def fizzbuzz():
+    for i in range(1, 101):
+        if(i % 15 == 0):
+            print("FizzBuzz", end=' ')
+        elif(i % 3 == 0):
+            print("Fizz", end=' ')
+        elif(i % 5 == 0):
+            print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
