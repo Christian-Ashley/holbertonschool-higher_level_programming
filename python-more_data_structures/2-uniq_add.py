3c6dcfbc725d6d4361eab1b9d116e67b88097479
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-09-12 20:28:24 +0000

    1

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set(my_list) #create set to store unique integers
+    sum_result = sum(unique_integers) #does math to unique integers
+    return sum_result
+    
