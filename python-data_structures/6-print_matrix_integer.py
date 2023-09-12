b7344d7b487061979ddda5df9d289ec408fd0c1d
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-09-12 17:51:52 +0000

    1

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    r = len(matrix)
+    c = len(matrix[0]) if matrix else 0
+
+    for r in matrix:
+        for i, element in enumerate(r):
+            if i == c - 1:
+                print("{:d}".format(element), end='')
+            else:
+                print("{:d}".format(element), end=' ')
+            print()
