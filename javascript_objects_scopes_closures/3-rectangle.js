ee15f527a72419863f6337c9081d8da285662528
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-11-27 08:15:52 +0000

    try this

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
+// Rectangle class with constructor and print method
 class Rectangle {
-  constructer (w, h) {
-    if (w >= 1 && h >= 1 && w !== undefined && h !== undefined) {
-      this.width = w;
-      this.height = h;
+    constructor (w, h) {
+      if (w >= 1 && h >= 1 && w !== undefined && h !== undefined) {
+        this.width = w;
+        this.height = h;
       }
     }
-
-    print(){
+  
+    print () {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
       }
     }
-}
-module.exports = Rectangle;
+  }
+  module.exports = Rectangle;
