09534ee1f576fc53a1a5db516a1c2a1ed35c5286
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-11-27 06:06:10 +0000

    task 1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// w is width h is height
+//we are making a rectangle here people its not rocket surgery
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle
