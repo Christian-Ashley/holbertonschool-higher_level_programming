2a04fa3f911d9d15579165ba25f9d96eba33d12e
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-11-27 08:10:12 +0000

    task 3

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,2 @@
+#!/usr/bin/node
+//same as 1 but with protections against wierd shit
---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+class Rectangle {
+  constructer (w, h) {
+    if (w >= 1 && h >= 1 && w !== undefined && h !== undefined) {
+      this.width = w;
+      this.height = h;
+      }
+    }
+    print(){
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+}
+module.exports = Rectangle;
