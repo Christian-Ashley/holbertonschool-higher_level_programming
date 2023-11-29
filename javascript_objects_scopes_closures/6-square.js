7808b91861f394e7886a24101b61f1570c5cc42f
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-11-29 05:22:46 +0000

    try that

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const square = require('./5-square');
 
 class Square extends square {
-    constructor (size) {
-        super(size, size);
-}
-    charPrint (c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            console.log(c.repeat(this.width));
-        }
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
