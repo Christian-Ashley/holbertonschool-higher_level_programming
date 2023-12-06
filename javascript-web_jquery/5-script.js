6f022e88f21434f8c25bacd912231ffe7aa97a64
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-12-06 00:27:25 +0000

    task 5

---
+++ javascript-web_jquery/5-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Write a JavaScript script that adds a <li> element to a list when the
+// user clicks on the tag DIV#add_item:
+
+$("DIV#add_item").click(function() {
+  $("UL.my_list").append("<li>Item</li>");
+}
+);
